92a8a67e3c629e1a20d3a260850ff4f725f13739
juan.02.12 <juan.02.12@gmail.com>
2013-10-19 19:53:59 +0000

    Busqueda cliente

--- MadeInHouse/ViewModels/Ventas/ClienteBuscarViewModel.cs
+++ MadeInHouse/ViewModels/Ventas/ClienteBuscarViewModel.cs
@@ -11,6 +11,7 @@ namespace MadeInHouse.ViewModels.Ventas
 {
     class ClienteBuscarViewModel : PropertyChangedBase
     {
+        private ClienteGateway cliGateway;
         private MyWindowManager win = new MyWindowManager();
 
         public void AbrirRegistrarcliente()
@@ -21,6 +22,52 @@ namespace MadeInHouse.ViewModels.Ventas
         public void AbrirEditarcliente()
         {
             win.ShowWindow(new Ventas.ClienteEditarViewModel { DisplayName = "Editar Cliente" });
+        }
+
+        public ClienteBuscarViewModel()
+        {
+            cliGateway = new ClienteGateway();
+            Clientes = cliGateway.BuscarClientes();
+        }
+
+        private List<Cliente> clientes;
+
+        public List<Cliente> Clientes
+        {
+            get
+            {
+                return this.clientes;
+            }
+
+            private set
+            {
+                if (this.clientes == value)
+                {
+                    return;
+                }
+
+                this.clientes = value;
+                this.NotifyOfPropertyChange(() => this.Clientes);
+            }
+        }
+
+        private Dictionary<string, int> sexo = new Dictionary<string, int>()
+        {
+            { "Seleccionar", -1 }, { "No sabe", 0 }, { "Masculino", 1 }, { "Femenino", 2 }, { "No aplica", 9 }
+        };
+
+        public BindableCollection<string> cmbSexo
+        {
+            get
+            {
+                return new BindableCollection<string>(sexo.Keys);
+            }
+        }
+
+        public void RealizarBusqueda(string dni, string telefono, string nombre, string cmbSexo)
+        {
+            cliGateway = new ClienteGateway();
+            Clientes = cliGateway.BuscarClientes(dni, telefono, nombre, sexo[cmbSexo]);
         }
     }
 }
