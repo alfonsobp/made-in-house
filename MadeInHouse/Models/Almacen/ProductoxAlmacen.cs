f840be9358d18c75f1da38f0d438f26bdc7d7eca
juan.02.12 <juan.02.12@gmail.com>
2013-11-01 00:54:20 +0000

    solicitud de abastecimiento

---
+++ MadeInHouse/Models/Almacen/ProductoxAlmacen.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadeInHouse.Models.Almacen
+{
+    class ProductoxAlmacen
+    {
+        public int idAlmacen { get; set; }
+        public int idProducto { get; set; }
+        public string producto { get; set; }
+        public int stock { get; set; }
+        public int stockMin { get; set; }
+        public int sugerido { get; set; }
+        public int pedido { get; set; }
+    }
+}
