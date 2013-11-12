503b744e0982d24a88d1ec9db16c044bca3a5e27
diego.garate <diego.garate@pucp.pe>
2013-11-12 05:57:11 +0000

    Se Cambio el Insert de Motivos y se controlo Enables de NI.

--- MadeInHouse/DataObjects/Almacen/MotivoSQL.cs
+++ MadeInHouse/DataObjects/Almacen/MotivoSQL.cs
@@ -19,7 +19,7 @@ namespace MadeInHouse.DataObjects.Almacen
             SqlCommand cmd = new SqlCommand();
             int k = 0;
 
-            cmd.CommandText = "INSERT INTO MotivoIS(nombre) VALUES (@motivo)";
+            cmd.CommandText = "INSERT INTO MotivoIS(nombre,tipo) VALUES (@motivo,3)";
             cmd.CommandType = CommandType.Text;
             cmd.Connection = conn;
             cmd.Parameters.AddWithValue("@motivo", m.NombreMotivo);
@@ -49,6 +49,41 @@ namespace MadeInHouse.DataObjects.Almacen
             SqlDataReader reader;
 
             cmd.CommandText = "SELECT * FROM MotivoIS";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = conn;
+
+            try
+            {
+                conn.Open();
+                reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Motivo m = new Motivo();
+                    m.Id = reader.IsDBNull(reader.GetOrdinal("idMotivo")) ? -1 : (int)reader["idMotivo"];
+                    m.NombreMotivo = reader.IsDBNull(reader.GetOrdinal("nombre")) ? null : reader["nombre"].ToString();
+                    listaMotivos.Add(m);
+                }
+
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.StackTrace.ToString());
+            }
+
+            return listaMotivos;
+        }
+
+        internal static List<Motivo> BuscarMotivos(int p)
+        {
+            List<Motivo> listaMotivos = new List<Motivo>(); ;
+            SqlConnection conn = new SqlConnection(Properties.Settings.Default.inf245g4ConnectionString);
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
+
+            cmd.CommandText = "SELECT * FROM MotivoIS WHERE tipo=@tipo OR tipo=3";
+            cmd.Parameters.AddWithValue("@tipo", p);
             cmd.CommandType = CommandType.Text;
             cmd.Connection = conn;
 
